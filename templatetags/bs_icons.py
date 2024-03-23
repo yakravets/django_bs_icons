a19ca89e4bf6076e77b4491db84139454eb82c7d
yakravets <30793298+yakravets@users.noreply.github.com>
2024-03-23 23:19:41 +0200

    add support linux platform

--- templatetags/bs_icons.py
+++ templatetags/bs_icons.py
@@ -1,4 +1,5 @@
 import os
+import platform
 from django import template
 from django.utils.html import format_html
 
@@ -7,7 +8,12 @@ register = template.Library()
 
 @register.simple_tag
 def bs_icon(icon_name):
-    cur_folder = os.path.abspath(__file__).replace('\\bs_icons.py', '')
-    icon_dir = os.path.join(cur_folder, 'icons\\')
+    if platform.system() == 'Windows':
+        cur_folder = os.path.abspath(__file__).replace('\\bs_icons.py', '')
+        icon_dir = os.path.join(cur_folder, 'icons\\')
+    else:
+        cur_folder = os.path.abspath(__file__).replace('/bs_icons.py', '')
+        icon_dir = os.path.join(cur_folder, 'icons/')
+            
     icon = open(icon_dir + icon_name + '.svg', 'r')
     return format_html(icon.read())
